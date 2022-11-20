638371bf48eb7943a43fa6b2ed2755e9f99f7165
HannahW234 <hannahwolken432@gmail.com>
2022-11-19 18:52:25 -0600

    Added credit table, customer_id, login or log out

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, session, request
 from user import User
 from userDatabase import *
-import tableDatabase as t
-
+from creditDatabase import * 
+import tableDatabase as t 
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 create_user_information_database()
+
 
 @app.route("/")
 def home():
@@ -21,9 +23,13 @@ def confirmation_page():
   email = request.form['email']
   password = request.form['password']
   user = User(name, email, password)
+  currentUserID = get_id(user)
+  user.set_id(currentUserID)
+  session['user'] = user.__dict__ ##allows for user to be used in other requests
+  if is_user_exist(user): 
+    session['logged_in'] = True
   
   return render_template("userloggedin.html", user_exist=is_user_exist(user))
-  
 
 
 @app.route('/registration', methods=["GET"])
@@ -34,7 +40,21 @@ def registration_page():
 def reservation_page():
   return render_template("reservation.html")
 
-@app.route('/payment', methods=["POST"])
+@app.route('/enter_payment', methods=["POST","GET"])
+def enter_payment():
+  name = request.form['credit_name']
+  credit_num = request.form['credit_num']
+  security = request.form['security_num']
+  exp_date = request.form['expiration_date']
+  
+  if session['logged_in']: 
+    currentUserID = session['user']['id']
+    credit_info = [currentUserID, name, credit_num, security, exp_date]
+    add_credit_card(credit_info)
+
+  return render_template("paymentConfirmation.html")
+
+@app.route('/payment', methods=["POST","GET"])
 def payment_page():
   return render_template("payment.html")
 
@@ -45,17 +65,25 @@ def creating_new_user_page():
   password = request.form['password']
   
   user = User(name, email, password)
-  add_user(user)
+  currentUserID = add_user(user)
+  user.set_id(currentUserID)
+  session['user'] = user.__dict__
+  session['logged_in'] = True
+  
   
   return render_template("userloggedin.html", user_exist=is_user_exist(user))
 
+@app.route('/logout', methods=['GET'])
+def logout():
+  session['logged_in'] = False
+  return render_template("index.html")
 
 @app.route('/table', methods=["POST"])
 def show_available_tables():
   time = request.form['time']
   date = request.form['date']
   size = request.form['size']
-
+  
   t.delete_ALL()
   t.create_table_information_database()
   print(t.fetchall())
@@ -74,4 +102,5 @@ def show_available_tables():
   return (f"{time[0:2]}")
 
 if __name__ == "__main__":
+  session.clear()
   app.run(debug=True)
--- user.py
+++ user.py
@@ -1,9 +1,19 @@
 class User:
   def __init__(self, name, email, password):
+    self.id = 0 
     self.name = name
     self.email = email
     self.password = password
-    
-    
+    self.reward_points = 0 
+
+  def add_points(self, points):
+    self.points = self.points+points
+  
+  def set_id(self, id): 
+    self.id = id 
+  
+  def get_id(self): 
+    return self.id
+
   def get_info(self):
-    return [self.name, self.email, self.password]
+    return [self.id, self.name, self.email, self.password]
